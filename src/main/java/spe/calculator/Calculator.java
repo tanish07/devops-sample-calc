bd948321a1b3e3d95072914933c59162a006730c
Tanish Chauhan <Tanish.Chauhan@iiitb.org>
2020-05-09 04:18:31 +0530

    Added addition and subtraction operations

--- src/main/java/spe/calculator/Calculator.java
+++ src/main/java/spe/calculator/Calculator.java
@@ -11,13 +11,49 @@ public class Calculator {
 		Scanner reader = new Scanner(System.in);
 		System.out.println("Calculator System");
 
-		System.out.println("Operations Menu");
-		System.out.println("");
-		System.out.println("1) Addition");
-		System.out.println("2) Subtraction");
-		System.out.println("3) Multiplication");
-		System.out.println("4) Division");
-		System.out.println("5) Exit");
+		do
+        {
+            System.out.println("Operations Menu");
+            System.out.println("");
+            System.out.println("1) Addition");
+            System.out.println("2) Subtraction");
+            System.out.println("3) Multiplication");
+            System.out.println("4) Division");
+            System.out.println("5) Exit");
+            System.out.print("Enter your choice: ");
+            ch = reader.nextInt();
+            if(ch==5)
+            {
+                flag = 1;
+            }
+            else
+            {
+                switch(ch)
+                {
+                case 1:
+                System.out.println("Addition");
+                System.out.println("Enter two numbers");
+                System.out.print("Enter number 1: ");
+                num1 = reader.nextDouble();
+                System.out.print("Enter number 2: ");
+                num2 = reader.nextDouble();
+                System.out.println(num1+num2);
+                break;
+                case 2:
+                System.out.println("Subtraction");
+                System.out.println("Enter two numbers");
+                System.out.print("Enter number 1: ");
+                num1 = reader.nextDouble();
+                System.out.print("Enter number 2: ");
+                num2 = reader.nextDouble();
+                System.out.println(num1-num2);
+                break;
+                default: System.out.println("Exiting program due to invalid input");
+                flag=1;
+                }
+            }
+            System.out.println("\n");
+            }while(flag==0);
 	}
 	
 }
