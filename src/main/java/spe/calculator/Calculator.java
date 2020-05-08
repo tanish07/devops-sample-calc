659ec85ee4cd394cd7aa0675018f14dc9cd96d8a
Tanish Chauhan <Tanish.Chauhan@iiitb.org>
2020-05-09 04:07:24 +0530

    Initial Commit

---
+++ src/main/java/spe/calculator/Calculator.java
@@ -0,0 +1,25 @@
+package spe.calculator;
+import java.util.*;
+
+
+public class Calculator {
+	
+	public static void main(String args[]) {
+
+		int flag = 0, ch;
+		double num1, num2;
+		Scanner reader = new Scanner(System.in);
+		System.out.println("Calculator System");
+
+		System.out.println("Operations Menu");
+		System.out.println("");
+		System.out.println("1) Addition");
+		System.out.println("2) Subtraction");
+		System.out.println("3) Multiplication");
+		System.out.println("4) Division");
+		System.out.println("5) Exit");
+	}
+	
+}
+	
+
